3d42cad61cba7c07c1f2f05c56045351b5326d92
Michal Fojtik <mfojtik@redhat.com>
2013-08-12 14:27:17 +0200

    Added redis config

--- workers.rb
+++ workers.rb
@@ -3,6 +3,21 @@
 
 require_relative './lib/workers/example_worker.rb'
 
+# NOTE: This require the Redis cartridge to be installed.
+#       https://github.com/smarterclayton/openshift-redis-cart
+
+REDIS_CONFIG = {
+  url: "redis://:#{ENV['REDIS_PASSWORD']}@#{ENV['OPENSHIFT_REDIS_HOST']}:#{ENV['OPENSHIFT_REDIS_PORT']}"
+}
+
+Sidekiq.configure_server do |config|
+  config.redis = REDIS_CONFIG
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = REDIS_CONFIG
+end
+
 # Example of how to call the task:
 #
 
